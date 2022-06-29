af9a81b0130f61fcff1d7673bb6fedae6884cf2a
a.ahmed <a.ahmed@fz-juelich.de>
2022-06-29 15:25:22 +0200

    SupplierDemo

--- src/main/java/com/webfluxdemo/functionalInterfaceDemo/SupplierDemo.java
+++ src/main/java/com/webfluxdemo/functionalInterfaceDemo/SupplierDemo.java
@@ -1,4 +1,31 @@
 package com.webfluxdemo.functionalInterfaceDemo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
 public class SupplierDemo {
+    /*@Override
+    public String get() {
+        return "HI hI";
+    }*/
+
+    public static void main(String[] args) {
+        /*SupplierDemo supplierDemo = new SupplierDemo();
+        System.out.println(supplierDemo.get());*/
+
+        //alternative way:
+        /*Supplier<String> supplier = new SupplierDemo();
+        System.out.println(supplier.get());*/
+
+        Supplier<String> supplier = ()->"Arif Ahmed";
+        System.out.println(supplier.get());
+
+        //List<String> list = Arrays.asList("arif", "ahmed");
+        List<String> list = Arrays.asList();
+
+        System.out.println(list.stream().findAny().orElseGet(()->"AA"));;
+
+
+    }
 }
